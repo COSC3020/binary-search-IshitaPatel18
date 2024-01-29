afa6b150bb06e175a6b5fc72e5634181530d2a76
Ishita Patel <144733216+IshitaPatel18@users.noreply.github.com>
2024-01-29 16:20:36 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,35 @@
-function binarySearch(list, element) {
+// Credit: Geeks for Geeks was used for inspiration and help
+
+function binarySearch(list, element)
+{
+    let left = 0;
+    let right = list.length - 1;
+    let mid;
+    
+    while(left <= right)
+    {
+       mid = Math.floor((right +left)/2);
+       if(list[mid] == element)
+       {
+           let first;
+           for(let i = mid; list[i] == element; i--)
+           {
+               if(list[i] == element)
+               {
+                   first = i;
+               }
+           }
+           return first;
+       }
+       else if(list[mid] < element)
+       {
+           left = mid + 1;
+       }
+       else
+       {
+           right = mid - 1;
+       }
+    }
+    
     return -1;
 }
